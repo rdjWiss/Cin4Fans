29a01da618e938a0ae784d6b7b46b88732f52d50
rdjWiss <33293992+rdjWiss@users.noreply.github.com>
2018-04-13 18:45:11 +0100

    Ajout de la liste du Crew + res

---
+++ app/src/main/java/prj/mob1/prjmob1/Crew.kt
@@ -0,0 +1,8 @@
+package prj.mob1.prjmob1
+
+/**
+ * Created by sol on 13/04/2018.
+ */
+data class Crew(val name:String, val character: String, val image: Int) {
+    constructor() : this("","",0)
+}
