7c829745511a206ca36060f83ee56d8ec4b39ecb
rdjWiss <33293992+rdjWiss@users.noreply.github.com>
2018-04-13 18:48:59 +0100

    Ajout des saisons d'un show

--- app/src/main/java/prj/mob1/prjmob1/show/ShowSeasonsFragment.kt
+++ app/src/main/java/prj/mob1/prjmob1/show/ShowSeasonsFragment.kt
@@ -1,25 +1,110 @@
 package prj.mob1.prjmob1.show
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import prj.mob1.prjmob1.R
+import prj.mob1.prjmob1.season.Season
 
 class ShowSeasonsFragment : Fragment() {
+
+    private lateinit var listener: ShowSeasonsFragment.OnSeasonSelected
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        if (context is OnSeasonSelected) {
+            listener = context
+        } else {
+            throw ClassCastException(context.toString() + " must implement OnSeasonSelected.")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater!!.inflate(R.layout.fragment_show_seasons, container, false)
+        val view: View =  inflater!!.inflate(R.layout.fragment_show_seasons, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.show_seasons_list) as
+                RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        var seasons: ArrayList<Season> = ArrayList<Season>()
+        val seasonNumsList = resources.getStringArray(R.array.seasons_nums)
+        val seasonEpsList = resources.getStringArray(R.array.seasons_eps)
+        val seasonDateBeginList = resources.getStringArray(R.array.seasons_date_begin)
+        val seasonDateEndList = resources.getStringArray(R.array.seasons_date_end)
+        val seasonOverviewsList = resources.getStringArray(R.array.seasons_overviews)
+        val titleShow= getString(R.string.season_title_show)
+        for(i in 0 .. seasonEpsList.size-1){
+            seasons.add(Season(seasonNumsList[i],titleShow, seasonEpsList[i].toInt(), seasonDateBeginList[i],
+                    seasonDateEndList[i],1,seasonOverviewsList[i]))
+
+        }
+
+        recyclerView.adapter = SeasonListAdapter(seasons)
+
+        return view
+    }
+
+    interface OnSeasonSelected {
+        fun onSeasonSelected(season: Season)
+    }
+    /**/
+    internal inner class SeasonListAdapter(val seasonList: ArrayList<Season>)
+        : RecyclerView.Adapter<prj.mob1.prjmob1.show.ShowSeasonsFragment.SeasonListAdapter.ViewHolder>() {
+
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+            val v = LayoutInflater.from(viewGroup.context)
+                    .inflate(R.layout.fragment_show_seasons_item, viewGroup, false)
+            return ViewHolder(v)
+        }
+
+        override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+            viewHolder.itemNums.text = seasonList[i].num_season
+            val seasonEp = " ${seasonList[i].nbr_episodes.toString()} episodes"//TODO: USE STRING
+            viewHolder.itemEps.text = seasonEp
+
+            viewHolder.itemView.setOnClickListener { v: View  ->
+
+                /*Snackbar.make(v, "Click detected on item $i",
+                        Snackbar.LENGTH_LONG).setAction("Action", null).show()
+*/
+                listener.onSeasonSelected(seasonList[i])
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return seasonList.size
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            var itemNums: TextView
+            var itemEps: TextView
+
+            init {
+                itemNums = itemView.findViewById(R.id.season_item_num)
+                itemEps = itemView.findViewById(R.id.season_item_eps)
+
+            }
+
+
+        }
     }
 }// Required empty public constructor
