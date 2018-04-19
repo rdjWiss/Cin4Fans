7459768a9395cc423191db986c67e84eb4d58897
rdjWiss <33293992+rdjWiss@users.noreply.github.com>
2018-04-19 19:13:14 +0100

    Ajout du splash screen

---
+++ app/src/main/java/prj/mob1/prjmob1/SplashActivity.kt
@@ -0,0 +1,43 @@
+package prj.mob1.prjmob1
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import prj.mob1.prjmob1.Home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 2000 //2 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+}
